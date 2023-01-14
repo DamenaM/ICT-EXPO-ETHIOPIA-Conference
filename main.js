6b26229a5f778a5a38ea60d9d643d9662f46fddb
Mtab <107020397+DamenaM@users.noreply.github.com>
2023-01-14 11:57:49 +0300

    fix comment

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ const projects = [
     name: 'Mulegeta Tameru',
     title: 'Regional Director',
     description:
-          'The Millennium Hall in Addis Ababa is the ideal location to host such ',
+          'The Millennium Hall support to host such ',
     'image icon': './image/chess.jpg',
     'image mobile': './image/jelela 2.jpeg',
     id: 2,
@@ -54,7 +54,7 @@ const projects = [
     name: 'Mulegeta Tameru',
     title: 'Head of Africa',
     description:
-          'World Economic Forum',
+          'World Economic Forum director',
     'image icon': './image/chess.jpg',
     'image mobile': './image/jelale 1.jpeg',
     id: 3,
@@ -65,27 +65,21 @@ projects.forEach((element, k) => {
   const myfeature = document.querySelector('.gridcontainer');
   const projectpopupCard = document.createElement('div');
 
-  const cardDetails = `<section class="featur-section">
+  const cardDetails = `<section class="features">
   
-  <div class="featurs">
-  <div>
       <div class="feature">
           <div class="feature-img">
               <img class="image-icon" src="${projects[k]['image icon']}" alt="">
               <img class="image-person" src="${projects[k]['image mobile']}" alt="">
           </div>
-          <div>
+          <div class="info">
               <h3>${projects[k].name}</h3>
               <p class="hardware">${projects[k].title}</p>
               <div class="line-feature"></div>
               <h4>${projects[k].description}</h4>
           </div>
       </div>
-  </div>
-</div>
-  
 </section>`;
-
   projectpopupCard.innerHTML += cardDetails;
   myfeature.append(projectpopupCard);
 });
